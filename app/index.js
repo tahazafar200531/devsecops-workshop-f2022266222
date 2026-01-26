7ea55e6154f5c461d513f274494596e72caae59d
tahazafar200531 <tahazafar200531@gmail.com>
2026-01-26 21:52:42 +0500

    Initial Commit

---
+++ app/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('DevSecOps Workshop Working!'));
+
+app.listen(3000, () => console.log('Running on port 3000'));
